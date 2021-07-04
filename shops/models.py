1e30f6710c063b9c9d08e3ea4aa0879e64c322d8
LorinLex <laizybot@mail.com>
2021-07-05 01:53:22 +0300

    Add & register models

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Street(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    house = models.CharField(max_length=10)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
