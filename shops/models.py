9cc3aed89a7478b89208d1114301a37315306962
LorinLex <laizybot@mail.com>
2021-07-05 09:26:57 +0300

    Rename foreign fields

--- shops/models.py
+++ shops/models.py
@@ -7,13 +7,13 @@ class City(models.Model):
 
 class Street(models.Model):
     name = models.CharField(max_length=50)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
 
 
 class Shop(models.Model):
     name = models.CharField(max_length=50)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    street = models.ForeignKey(Street, on_delete=models.CASCADE)
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
+    street_id = models.ForeignKey(Street, on_delete=models.CASCADE)
     house = models.CharField(max_length=10)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
