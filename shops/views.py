c984f53d7854ae59c2fc875a195a9e56db6af422
LorinLex <laizybot@mail.com>
2021-07-05 14:25:24 +0300

    Added some swagger

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,4 @@
+from django.utils.decorators import method_decorator
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -6,6 +7,7 @@ from .models import City, Street, Shop
 from .serializers import CitySerializer, StreetSerializer, \
     ShopListSerializer, ShopCreateSerializer
 from .filters import ShopFilter
+from drf_yasg.utils import swagger_auto_schema
 
 
 class CityViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
@@ -13,12 +15,28 @@ class CityViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = CitySerializer
 
     @action(methods=['GET'], detail=True)
+    @swagger_auto_schema(
+        responses={
+            200: StreetSerializer(many=True),
+            400: "{\"detail\": \<error\>}",
+        }
+    )
     def street(self, request, pk):
-        queryset = Street.objects.select_related('city').filter(city_id=pk)
-        serializer = StreetSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        try:
+            queryset = Street.objects.select_related('city').filter(city_id=pk)
+            serializer = StreetSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ValueError as e:
+            return Response({'detail': str(e)},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    responses={
+        200: "{\"id\": \<new shop id\>}",
+        400: "{\<field\>: \<error\>}",
+    }
+))
 class ShopViewSet(viewsets.GenericViewSet,
                   mixins.CreateModelMixin,
                   mixins.ListModelMixin):
